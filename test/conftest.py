8f818dc94a3bfcf03754a9db66c7be34402e5080
David Steele <dsteele@gmail.com>
2018-08-09 17:29:19 -0400

    More test rework

--- test/conftest.py
+++ test/conftest.py
@@ -2,18 +2,24 @@ import os
 from collections import namedtuple
 import py
 
-Case = namedtuple("Case", ["infile", "outfile"])
+
+def cases():
+    Case = namedtuple("Case", ["infile", "outfile"])
+    casepath = "test/cases/"
+
+    cases = []
+    for path in os.listdir(casepath):
+        if path[-3:] == ".in":
+            cases.append(
+                Case(
+                    py.path.local(casepath + path),
+                    py.path.local(casepath + path[:-3] + ".out"),
+                )
+            )
+
+    return cases
 
 
 def pytest_generate_tests(metafunc):
     if 'file_case' in metafunc.fixturenames:
-        cases = []
-        for path in os.listdir("test/cases/"):
-            if path[-3:] == ".in":
-                cases.append(
-                    Case(
-                        py.path.local("test/cases/" + path),
-                        py.path.local("test/cases/" + path[:-3] + ".out"),
-                    )
-                )
-        metafunc.parametrize('file_case', cases)
+        metafunc.parametrize('file_case', cases())
