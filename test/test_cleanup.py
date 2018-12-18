9a1f81e3d3352202e7e6cd3a9103462ef8086675
David Steele <davesteele@users.noreply.github.com>
2018-12-18 11:15:43 -0500

    test-cleanup: Only need to read test output once

--- test/test_cleanup.py
+++ test/test_cleanup.py
@@ -9,7 +9,7 @@ def test_cleanup(clean_fxt, numruns):
     for _ in range(numruns):
         tdtcleanup.cleanup(clean_fxt.workfile)
 
-        test_output = clean_fxt.workfile.read_text("utf-8")
-        ref_output = clean_fxt.outfile.read_text("utf-8")
+    test_output = clean_fxt.workfile.read_text("utf-8")
+    ref_output = clean_fxt.outfile.read_text("utf-8")
     assert(test_output == ref_output)
     assert(clean_fxt.workfile.size() == clean_fxt.outfile.size())
