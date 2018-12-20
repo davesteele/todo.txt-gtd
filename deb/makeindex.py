8149400432bf8dfaf746c6c2e3500e0f2b45cc39
David Steele <dsteele@gmail.com>
2018-12-19 20:08:39 -0500

    Make an index.html for the deb dir

---
+++ deb/makeindex.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Build index from directory listing
+
+make_index.py </path/to/directory> [--header <header text>]
+"""
+
+INDEX_TEMPLATE = r"""
+<html>
+<body>
+<h2>Todo.txt-gtd Deb Files</h2>
+<p>
+% for name in names:
+    <li><a href="${name}">${name}</a></li>
+% endfor
+</p>
+</body>
+</html>
+"""
+
+EXCLUDED = ['index.html', 'makeindex.py']
+
+import os
+import argparse
+
+# May need to do "pip install mako"
+from mako.template import Template
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory")
+    parser.add_argument("--header")
+    args = parser.parse_args()
+    fnames = [fname for fname in sorted(os.listdir(args.directory))
+              if fname not in EXCLUDED]
+    header = (args.header if args.header else os.path.basename(args.directory))
+    print(Template(INDEX_TEMPLATE).render(names=fnames, header=header))
+
+
+if __name__ == '__main__':
+    main()
+
